54572e74b26bdb07c852d8ad122c8b740f38d7d5
Noroetry <lorenzo.martinez.pablo@gmail.com>
2023-11-12 11:04:01 +0100

    First table of data

--- index.js
+++ index.js
@@ -36,3 +36,61 @@ axios.get(`https://api-fantasy.llt-services.com/api/v3/leagues/${JOJU_LEAGUE}/te
   .catch(error => {
     console.error(error);
   });
+
+// GET STATUS FROM MY TEAM
+axios.get(`https://api-fantasy.llt-services.com/api/v3/leagues/${JOJU_LEAGUE}/teams`, config)
+  .then(response => {
+    if (response.status === 200){
+        teams = response.data
+    }
+    else{
+        console.log(`2 - The request's resolution isnt correctly. Status code: ${response.status}`)
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  })
+  .then(() => {
+    let team = teams.find((user) => user["id"] == myTeam.id);
+    team["players"].sort((a, b) => {
+      if (a["playerMaster"]["positionId"] < b["playerMaster"]["positionId"]){
+        return -1;
+      }
+      else if (a["playerMaster"]["positionId"] > b["playerMaster"]["positionId"]){
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    });
+    team["players"].forEach((p) => {
+      if (p["playerMaster"]["positionId"] === 1){
+        p["playerMaster"].positionDescription = "POR";
+      }
+      else if (p["playerMaster"]["positionId"] === 2){
+        p["playerMaster"].positionDescription = "DEF";
+      }
+      else if (p["playerMaster"]["positionId"] === 3){
+        p["playerMaster"].positionDescription = "CEN";
+      }
+      else {
+        p["playerMaster"].positionDescription = "DEL";
+      }
+
+      let formatDate = new Date(p["buyoutClauseLockedEndTime"]);
+      let actualDate = new Date();
+      if (formatDate > actualDate){
+        p.clauseBlocked = "SI";
+      }
+      else {
+        p.clauseBlocked = "NO";
+      }
+    });
+    var del = "\t|\t";
+    for (let player of team.players){
+      console.log(player["playerMaster"]["positionDescription"] + del + player["playerMaster"]["nickname"] + del + "Points: " + player["playerMaster"]["points"] + del +
+                  "Status: " + player["playerMaster"]["playerStatus"] + del + " Bloqueo: "+ player["clauseBlocked"] + del + player["buyoutClauseLockedEndTime"] + "\n" +
+                  "Tendencia: "
+      );
+    }
+  });
