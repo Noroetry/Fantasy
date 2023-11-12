5244152eca46e1700026b28c711a2273075cfc9d
Noroetry <lorenzo.martinez.pablo@gmail.com>
2023-11-12 11:16:28 +0100

    All teams tables

--- index.js
+++ index.js
@@ -35,6 +35,56 @@ axios.get(`https://api-fantasy.llt-services.com/api/v3/leagues/${JOJU_LEAGUE}/te
   })
   .catch(error => {
     console.error(error);
+  })
+  .then(() => {
+    teams.forEach((team) => {
+      team["players"].sort((a, b) => {
+        if (a["playerMaster"]["positionId"] < b["playerMaster"]["positionId"]){
+          return -1;
+        }
+        else if (a["playerMaster"]["positionId"] > b["playerMaster"]["positionId"]){
+          return 1;
+        }
+        else {
+          return 0;
+        }
+      });
+      team["players"].forEach((p) => {
+        if (p["playerMaster"]["positionId"] === 1){
+          p["playerMaster"].positionDescription = "POR";
+        }
+        else if (p["playerMaster"]["positionId"] === 2){
+          p["playerMaster"].positionDescription = "DEF";
+        }
+        else if (p["playerMaster"]["positionId"] === 3){
+          p["playerMaster"].positionDescription = "CEN";
+        }
+        else {
+          p["playerMaster"].positionDescription = "DEL";
+        }
+  
+        let formatDate = new Date(p["buyoutClauseLockedEndTime"]);
+        let actualDate = new Date();
+        if (formatDate > actualDate){
+          p.clauseBlocked = "SI";
+        }
+        else {
+          p.clauseBlocked = "NO";
+        }
+      });
+    });
+  })
+  .then(() => {
+    var del = "\t|\t";
+    for (let user of teams){
+      console.log(user["manager"]["managerName"]);
+      for (let player of user.players){
+      console.log(player["playerMaster"]["positionDescription"] + del + player["playerMaster"]["nickname"] + del + "Points: " + player["playerMaster"]["points"] + del +
+                  "Status: " + player["playerMaster"]["playerStatus"] + del + " Bloqueo: "+ player["clauseBlocked"] + del + player["buyoutClauseLockedEndTime"] 
+      );
+      }
+      console.log("------------------------");
+    }
   });
 
 // GET STATUS FROM MY TEAM
@@ -49,48 +99,13 @@ axios.get(`https://api-fantasy.llt-services.com/api/v3/leagues/${JOJU_LEAGUE}/te
   })
   .catch(error => {
     console.error(error);
-  })
-  .then(() => {
-    let team = teams.find((user) => user["id"] == myTeam.id);
-    team["players"].sort((a, b) => {
-      if (a["playerMaster"]["positionId"] < b["playerMaster"]["positionId"]){
-        return -1;
-      }
-      else if (a["playerMaster"]["positionId"] > b["playerMaster"]["positionId"]){
-        return 1;
-      }
-      else {
-        return 0;
-      }
-    });
-    team["players"].forEach((p) => {
-      if (p["playerMaster"]["positionId"] === 1){
-        p["playerMaster"].positionDescription = "POR";
-      }
-      else if (p["playerMaster"]["positionId"] === 2){
-        p["playerMaster"].positionDescription = "DEF";
-      }
-      else if (p["playerMaster"]["positionId"] === 3){
-        p["playerMaster"].positionDescription = "CEN";
-      }
-      else {
-        p["playerMaster"].positionDescription = "DEL";
-      }
-
-      let formatDate = new Date(p["buyoutClauseLockedEndTime"]);
-      let actualDate = new Date();
-      if (formatDate > actualDate){
-        p.clauseBlocked = "SI";
-      }
-      else {
-        p.clauseBlocked = "NO";
-      }
-    });
-    var del = "\t|\t";
+  });
+/*
+var del = "\t|\t";
     for (let player of team.players){
       console.log(player["playerMaster"]["positionDescription"] + del + player["playerMaster"]["nickname"] + del + "Points: " + player["playerMaster"]["points"] + del +
                   "Status: " + player["playerMaster"]["playerStatus"] + del + " Bloqueo: "+ player["clauseBlocked"] + del + player["buyoutClauseLockedEndTime"] + "\n" +
                   "Tendencia: "
       );
     }
-  });
+*/
